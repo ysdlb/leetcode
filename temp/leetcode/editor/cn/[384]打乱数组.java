fe2dd364b4dfa14226aeb1a515172000db2adf3d
gqhan <libertyhan@outlook.com>
2022-04-10 19:59:07 +0800

    打乱数组

---
+++ temp/leetcode/editor/cn/[384]打乱数组.java
@@ -0,0 +1,86 @@
+//给你一个整数数组 nums ，设计算法来打乱一个没有重复元素的数组。打乱后，数组的所有排列应该是 等可能 的。 
+//
+// 实现 Solution class: 
+//
+// 
+// Solution(int[] nums) 使用整数数组 nums 初始化对象 
+// int[] reset() 重设数组到它的初始状态并返回 
+// int[] shuffle() 返回数组随机打乱后的结果 
+// 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入
+//["Solution", "shuffle", "reset", "shuffle"]
+//[[[1, 2, 3]], [], [], []]
+//输出
+//[null, [3, 1, 2], [1, 2, 3], [1, 3, 2]]
+//
+//解释
+//Solution solution = new Solution([1, 2, 3]);
+//solution.shuffle();    // 打乱数组 [1,2,3] 并返回结果。任何 [1,2,3]的排列返回的概率应该相同。例如，返回 [3, 
+//1, 2]
+//solution.reset();      // 重设数组到它的初始状态 [1, 2, 3] 。返回 [1, 2, 3]
+//solution.shuffle();    // 随机返回数组 [1, 2, 3] 打乱后的结果。例如，返回 [1, 3, 2]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 200 
+// -10⁶ <= nums[i] <= 10⁶ 
+// nums 中的所有元素都是 唯一的 
+// 最多可以调用 5 * 10⁴ 次 reset 和 shuffle 
+// 
+// Related Topics 数组 数学 随机化 👍 273 👎 0
+
+
+import java.util.Arrays;
+import java.util.Random;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution384 {
+
+
+    private final int[] origin;
+    private final Random random;
+
+    public Solution384(int[] nums) {
+        origin = nums;
+        random = new Random();
+    }
+
+    public int[] reset() {
+        return origin;
+    }
+
+    public int[] shuffle() {
+        int[] shuffle = Arrays.copyOf(origin, origin.length);
+        for (int i = 0; i < shuffle.length; i++) {
+            // int r = random.nextInt(shuffle.length);
+            int r = i + random.nextInt(shuffle.length - i);
+            this.swap(shuffle, i, r);
+        }
+        return shuffle;
+    }
+
+    private void swap(int[] nums, int a, int b) {
+        if (a == b) return;
+        int t = nums[a];
+        nums[a] = nums[b];
+        nums[b] = t;
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * int[] param_1 = obj.reset();
+ * int[] param_2 = obj.shuffle();
+ */
+//leetcode submit region end(Prohibit modification and deletion)

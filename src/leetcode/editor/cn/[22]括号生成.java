01bdabdeffa6ea5dbe7e2a7ad65312be0c86b615
gqhan <libertyhan@outlook.com>
2022-04-15 11:49:26 +0800

    括号匹配：平衡法比栈更优秀

--- src/leetcode/editor/cn/[22]括号生成.java
+++ src/leetcode/editor/cn/[22]括号生成.java
@@ -34,6 +34,7 @@ class Solution22 {
     /**
      * 全排列问题, 参考 46, 47 题
      * 只有左括号的数量比右括号的数量多的时候, 才能添加右括号, 否则只能添加左括号
+     * 括号匹配规则参考: <a href="https://leetcode-cn.com/problems/minimum-add-to-make-parentheses-valid/">leetcode-921</a>
      */
     public List<String> generateParenthesis(int n) {
         List<String> ret = new ArrayList<>();

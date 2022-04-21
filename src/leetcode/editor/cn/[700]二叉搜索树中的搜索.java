7c4339f7b11e64a0e733a040f97b15b6ef782550
gqhan <libertyhan@outlook.com>
2022-04-22 07:09:24 +0800

    Update [700]二叉搜索树中的搜索.java

--- src/leetcode/editor/cn/[700]二叉搜索树中的搜索.java
+++ src/leetcode/editor/cn/[700]二叉搜索树中的搜索.java
@@ -27,22 +27,11 @@
 
 
 //leetcode submit region begin(Prohibit modification and deletion)
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution700 {
+    /**
+     * 二叉搜索树的性质
+     * 基础题，类似简单题：98
+     */
     public TreeNode searchBST(TreeNode root, int val) {
         if (root == null) return null;
 

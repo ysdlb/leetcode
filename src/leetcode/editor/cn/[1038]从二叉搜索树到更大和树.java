88fe092e36d8b8a38b283ba8f205eada0aa22e79
gqhan <libertyhan@outlook.com>
2022-04-22 07:26:48 +0800

    二叉搜索树：逆向中序遍历

--- src/leetcode/editor/cn/[1038]从二叉搜索树到更大和树.java
+++ src/leetcode/editor/cn/[1038]从二叉搜索树到更大和树.java
@@ -1,7 +1,6 @@
-//给出二叉 搜索 树的根节点，该树的节点值各不相同，请你将其转换为累加树（Greater Sum Tree），使每个节点 node 的新值等于原树中大于或等于
-// node.val 的值之和。 
+//给定一个二叉搜索树 root (BST)，请将它的每个节点的值替换成树中大于或者等于该节点值的所有节点值之和。 
 //
-// 提醒一下，二叉搜索树满足下列约束条件： 
+// 提醒一下， 二叉搜索树 满足下列约束条件： 
 //
 // 
 // 节点的左子树仅包含键 小于 节点键的节点。 
@@ -9,35 +8,22 @@
 // 左右子树也必须是二叉搜索树。 
 // 
 //
-// 注意：本题和 1038: https://leetcode-cn.com/problems/binary-search-tree-to-greater-
-//sum-tree/ 相同 
-//
 // 
 //
 // 示例 1： 
 //
 // 
 //
-// 输入：[4,1,6,0,2,5,7,null,null,null,3,null,null,null,8]
+// 
+//输入：[4,1,6,0,2,5,7,null,null,null,3,null,null,null,8]
 //输出：[30,36,21,36,35,26,15,null,null,null,33,null,null,null,8]
 // 
 //
 // 示例 2： 
 //
-// 输入：root = [0,null,1]
+// 
+//输入：root = [0,null,1]
 //输出：[1,null,1]
-// 
-//
-// 示例 3： 
-//
-// 输入：root = [1,0,2]
-//输出：[3,3,2]
-// 
-//
-// 示例 4： 
-//
-// 输入：root = [3,2,4,1]
-//输出：[7,9,4,10]
 // 
 //
 // 
@@ -45,21 +31,25 @@
 // 提示： 
 //
 // 
-// 树中的节点数介于 0 和 10⁴ 之间。 
-// 每个节点的值介于 -10⁴ 和 10⁴ 之间。 
-// 树中的所有值 互不相同 。 
-// 给定的树为二叉搜索树。 
+// 树中的节点数在 [1, 100] 范围内。 
+// 0 <= Node.val <= 100 
+// 树中的所有值均 不重复 。 
 // 
-// Related Topics 树 深度优先搜索 二叉搜索树 二叉树 👍 595 👎 0
+//
+// 
+//
+// 注意：该题目与 538: https://leetcode-cn.com/problems/convert-bst-to-greater-tree/ 相同
+// 
+// Related Topics 树 深度优先搜索 二叉搜索树 二叉树 👍 161 👎 0
 
 
 //leetcode submit region begin(Prohibit modification and deletion)
-class Solution538 {
+class Solution1038 {
     /**
      * 逆向中序遍历
-     * 同题 1038
+     * 同题 538
      */
-    public TreeNode convertBST(TreeNode root) {
+    public TreeNode bstToGst(TreeNode root) {
         convert(root);
         return root;
     }

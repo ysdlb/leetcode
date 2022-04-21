9e1609d7fbc2dee308d9bdd9157521d00ce9b70f
gqhan <libertyhan@outlook.com>
2022-04-21 12:28:32 +0800

    图：同二叉树，N叉树的深度和遍历

---
+++ src/leetcode/editor/cn/[559]N 叉树的最大深度.java
@@ -0,0 +1,96 @@
+//给定一个 N 叉树，找到其最大深度。 
+//
+// 最大深度是指从根节点到最远叶子节点的最长路径上的节点总数。 
+//
+// N 叉树输入按层序遍历序列化表示，每组子节点由空值分隔（请参见示例）。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//
+// 
+//输入：root = [1,null,3,2,4,null,5,6]
+//输出：3
+// 
+//
+// 示例 2： 
+//
+// 
+//
+// 
+//输入：root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,
+//null,13,null,null,14]
+//输出：5
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树的深度不会超过 1000 。 
+// 树的节点数目位于 [0, 10⁴] 之间。 
+// 
+// Related Topics 树 深度优先搜索 广度优先搜索 👍 268 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+import java.util.List;
+
+class Solution559 {
+    /**
+     * 同二叉树的最大深度 104
+     * 1. 层序遍历
+     * 2. DFS 后续遍历
+     * 这里是方法 2 的实现
+     */
+    public int maxDepth(Node root) {
+        if (root == null) return 0;
+        if (root.children == null || root.children.isEmpty())
+            return 1;
+
+        int max = 0;
+        for (Node node: root.children) {
+            max = Math.max(maxDepth(node), max);
+        }
+        return max + 1;
+    }
+
+    private static class Node {
+        public int val;
+        public List<Node> children;
+
+        public Node() {}
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, List<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)

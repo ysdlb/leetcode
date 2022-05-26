9c7e4a757d72783218d0fd2f152a84f22f03718a
gqhan <libertyhan@outlook.com>
2022-05-26 11:02:31 +0800

    链表：简单，拆分后合并，时间O(n)，空间O(1)

---
+++ src/leetcode/editor/cn/[328]奇偶链表.java
@@ -0,0 +1,80 @@
+//给定单链表的头节点 head ，将所有索引为奇数的节点和索引为偶数的节点分别组合在一起，然后返回重新排序的列表。 
+//
+// 第一个节点的索引被认为是 奇数 ， 第二个节点的索引为 偶数 ，以此类推。 
+//
+// 请注意，偶数组和奇数组内部的相对顺序应该与输入时保持一致。 
+//
+// 你必须在 O(1) 的额外空间复杂度和 O(n) 的时间复杂度下解决这个问题。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//
+// 
+//输入: head = [1,2,3,4,5]
+//输出: [1,3,5,2,4] 
+//
+// 示例 2: 
+//
+// 
+//
+// 
+//输入: head = [2,1,3,5,6,4,7]
+//输出: [2,3,6,7,1,5,4] 
+//
+// 
+//
+// 提示: 
+//
+// 
+// n == 链表中的节点数 
+// 0 <= n <= 10⁴ 
+// -10⁶ <= Node.val <= 10⁶ 
+// 
+// Related Topics 链表 👍 587 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution328 {
+
+    /**
+     * 双指针, 奇链表指针 + 偶链表指针
+     * 直接一次遍历将链表按索引拆成奇偶两个链表
+     * 然后将偶数链表连接到奇链表的后面
+     */
+    public ListNode oddEvenList(ListNode head) {
+        ListNode odd = new ListNode();
+        ListNode even = new ListNode();
+        ListNode evenHead = even;
+
+        ListNode cur = head;
+        for (int i = 1; cur != null; i++) {
+            // 奇数
+            if ((i & 1) == 1) {
+                odd.next = cur;
+                odd = odd.next;
+            } else {
+                even.next = cur;
+                even = even.next;
+            }
+
+            cur = cur.next;
+        }
+
+        odd.next = evenHead.next;
+        even.next = null;
+
+        return head;
+    }
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)

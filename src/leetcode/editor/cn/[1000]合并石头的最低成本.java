3ab7ee0ad29147ba76e743d35b78ecb206ab27c2
gqhan <libertyhan@outlook.com>
2023-04-06 21:36:34 +0800

    Update [1000]合并石头的最低成本.java

--- src/leetcode/editor/cn/[1000]合并石头的最低成本.java
+++ src/leetcode/editor/cn/[1000]合并石头的最低成本.java
@@ -50,18 +50,40 @@
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution1000 {
-    /**
+    /* 合并石头的最低成本: https://leetcode.cn/problems/minimum-cost-to-merge-stones/
+     * 相似题:
+     *   312. 戳气球: https://leetcode.cn/problems/burst-balloons/
+     *
      * 每次合并总会减少 k-1 堆石头, 最后一次减少 k 堆石头
      * (k-1) * (n-1) + k = total ==> (k-1)*n = total - 1
      *
-     * 首先简化问题: 假设 k = 2
-     * 则可以设置 f(i,j) 为 合并 [i,j] 区间内石头为 1 堆的最低成本
-     * f(i,j) = min{f(i,m) + f(m+1,j) + sum(i,j)}   i <= m <= j
+     * 每次都是将连续的 k 堆合并为 1 堆
      *
-     * 现在 k >= 2
-     * 设置 f(i,j,x) 为合并 [i,j] 区间内石头为 x 堆的最低成本
-     * f(i,j,1) = f(i,j,k) + sum(i,j)
-     * f(i,j,x) = min{f(i,m,1) + f(m+1,j,x-1)}, 2 <= x <= k, i <= m < j; m += k-1
+     * 首先简化问题: 假设 k = 2
+     * 则可以设置 f(i,j) 为 合并 [i,j] 区间内的两堆石头为 1 堆的最低成本
+     * 我们设 i <= m < j，总是可以将 [i,j] 分为两堆
+     *   1. [i,m] 合并成的一堆
+     *   2. [m+1,j] 合并成的一堆
+     * 遍历 m，求:
+     *   f(i,j) = min{f(i,m)+f(m+1,j), ...} + sum(i,j)   i <= m < j
+     *
+     * 假设 f(i,j,x) 为合并 [i,j] 区间内石头为 x 堆的最低成本，我们要将 [i,j] 合并成一堆，则需要之前 [i,j] 内已经合并成了 k 堆
+     *   f(i,j,1) = f(i,j,k) + sum(i,j)
+     * 若 k == 2，
+     *   min{f(i,m,1)+f(m+1,j,1), ...} == f(i,j,2)
+     *
+     *
+     * 若 k >= 2; 且 x >= 2; 遍历 m 求:
+     *   f(i,j,x) = min{f(i,m,1)+f(m+1,j,x-1), ...},  2 <= x <= k, i <= m < j; m += k-1
+     * 结合 f(i,j,1) = f(i,j,k) + sum(i,j), 我们可以推出上面 k == 2 时特殊情况的公式
+     *
+     * 若 k != 2，[i,j] 不一定可以满足每次合并 k 个的要求，在这个要求的基础上，[i,j] 也不一定可以满足合并为 x 堆的要求
+     * 对于这些不可能的 f(i,j,x) 值，我们设置为正无穷
+     *
+     * 同时遍历的过程中，可以酌情将 m 每一跳的间隙设置为 k-1，来忽略不可能到达的位置
+     * 因为在每次合并 k 堆的基础上，若 [i,m] 可以合并为 1 堆，[m+1,j] 必然可以合并为 k-1 堆
+     *
+     * [i,j] 循环中，f(i,j,x) (2<=x) 因为没有 1 堆的限定条件，所以不可以这么跳
      *
      * 初始状态推断
      * 如果 j-i+1 == k, 则 f(i,j,k) == 0 ==> f(i,i,1) == 0

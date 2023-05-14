d2c996125670453689768386f52e0c709965fc61
gqhan <libertyhan@outlook.com>
2023-05-14 10:31:43 +0800

    Update [3]无重复字符的最长子串.java
    整理下写法

--- src/leetcode/editor/cn/[3]无重复字符的最长子串.java
+++ src/leetcode/editor/cn/[3]无重复字符的最长子串.java
@@ -42,14 +42,21 @@ import java.util.Arrays;
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution3 {
-    /**
-     * 无重复元素的最长字串, 不是子序列
+    /* 无重复字符的最长子串: https://leetcode.cn/problems/longest-substring-without-repeating-characters/
+     * 滑动窗口相似题目:
+     *  2106.摘水果: https://leetcode.cn/problems/maximum-fruits-harvested-after-at-most-k-steps/     * 字串, 不是子序列
+     *
+     * 区间和不涉及负数，优先用滑动窗口解决
+     *
      * 遍历字符串
      * 1. 如果 c 在集合中, 从集合中删除 c 之前包括 c 的所有元素;
      * 2. 将 c 加入集合
      *
      * 因为是窗口, 所以不用一个一个删, 这样时间复杂度就是 O(n) 了
-     * 期间集合的最大容量就是符合要求的最长字串长度
+     * 窗口内一直是不重复的，所以可以用窗口代表集合,
+     *  map[ch] 表示 ch 上次出现的位置
+     *  若 l <= map[ch] <= r, 则表示 ch 已经在窗口中了，窗口需要收缩 l <= map[ch]+1
+     * 期间窗口的最大范围就是结果
      */
     public int lengthOfLongestSubstring_v2(String s) {
         int[] map = new int[128];
@@ -60,18 +67,16 @@ class Solution3 {
         while (r < s.length()) {
             int ch = s.charAt(r);
             // 如果集合中存在
-            if (l <= map[ch] && map[ch] <= r) {
-                int end = map[ch];
-                if (l <= end)
-                    l = end+1;
-            }
+            if (l <= map[ch] && map[ch] <= r)
+                l = map[ch]+1;
             map[ch] = r;
             ret = Math.max(ret, r-l+1);
             r++;
         }
         return ret;
     }
-    /**
+
+    /*
      * 无重复元素的最长字串, 不是子序列
      * 遍历字符串
      * 1. 如果 c 在集合中, 从集合中删除 c 之前包括 c 的所有元素;
